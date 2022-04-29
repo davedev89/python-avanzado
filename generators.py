8a36b6a8cca16e32436f5710874148ac810e17bd
davedev89 <davidcarrevedo@gmail.com>
2022-04-29 12:28:38 -0300

    generators / iterators

---
+++ generators.py
@@ -0,0 +1,25 @@
+import time
+
+def fibo_gen(stop):
+     n1 = 0
+     n2 = 1
+     counter = 0
+     max = stop
+     while counter < stop :
+          if counter == 0:
+               counter += 1
+               yield n1
+          elif counter == 1:
+               counter += 1
+               yield n2
+          else:
+               aux = n1 + n2
+               n1, n2 = n2, aux #swap
+               counter += 1
+               yield aux
+
+if __name__ == '__main__':
+     fibonacci = fibo_gen(15)
+     for element in fibonacci:
+          print(element)
+          time.sleep(0.3)
